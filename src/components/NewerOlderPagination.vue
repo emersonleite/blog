dc4a83870826d63e198a6353e4b03e5001f6e8ac
Emerson Leite <emerson.bl@gmail.com>
2025-11-15 11:15:55 -0300

    chore: migrate blog repo

---
+++ src/components/NewerOlderPagination.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex justify-center gap-8">
+    <a v-if="page.url.prev" :href="page.url.prev">← Posts mais recentes (Newer Posts)</a>
+    <a v-if="page.url.next" :href="page.url.next">Posts mais antigos (Older Posts) →</a>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(['page']);
+</script>
---
+++ src/utils/AppConfig.js
@@ -0,0 +1,8 @@
+export const AppConfig = {
+  site_name: 'Hey Mike!',
+  title: 'Hey Mike!',
+  description: 'Site e blog de Emerson Leite',
+  author: 'Emerson Leite',
+  locale_region: 'pt-br',
+  locale: 'pt',
+};
